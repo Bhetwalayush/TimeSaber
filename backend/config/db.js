4fd664d6d4ea7768b46fc26c7d7780a709ef63c9
BhetwalAyush <rujanbhetwal@gmail.com>
2025-07-29 19:19:48 +0545

    database

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+//1.Importing th e package
+const mongoose = require('mongoose');
+
+//2. Creating a function
+const connectDB = () => {
+    mongoose.connect(process.env.MONGODB_URL).then(() => {
+        console.log("Database Connected Sucessfully")
+    })
+}
+//3.Exporting the function
+module.exports = connectDB;
