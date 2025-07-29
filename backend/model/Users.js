333f559f116900175bf9d916f54537d2b549b3f6
BhetwalAyush <rujanbhetwal@gmail.com>
2025-07-29 19:23:00 +0545

    auth

---
+++ backend/model/Users.js
@@ -0,0 +1,54 @@
+const { number } = require("joi");
+const mongoose = require("mongoose");
+
+const usersSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+       
+    },
+    region_state: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    confirm_password: {
+        type: String,
+        required: true
+    },
+    profile_picture: {
+        type: String,
+        // required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    }
+
+});
+
+const User = mongoose.model("user", usersSchema);
+
+module.exports = User;
